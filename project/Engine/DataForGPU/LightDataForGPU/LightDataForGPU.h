b333f022461868b368ede02700565d1d8897ef98
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-02-09 21:37:43 +0900

    一応コミット

---
+++ project/Engine/DataForGPU/LightDataForGPU/LightDataForGPU.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Engine
+{
+	/// @brief GPUに送る平行光源
+	struct DirectionalLightDataForGPU
+	{
+
+	};
+
+	/// @brief GPUに送るポイントライト
+	struct PointLightDataForGPU
+	{
+
+	};
+
+	/// @brief GPUに送るスポットライト
+	struct SpotLightDataForGPU
+	{
+
+	};
+}
