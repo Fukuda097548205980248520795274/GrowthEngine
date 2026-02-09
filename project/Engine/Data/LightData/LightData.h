a8b59197c8add7d711637d2aeee9e566f64a3b59
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-02-09 21:31:03 +0900

    一応コミット

--- project/Engine/Data/LightData/LightData.h
+++ project/Engine/Data/LightData/LightData.h
@@ -6,6 +6,9 @@ namespace Engine
 	/// @brief 平行光源パラメータ
 	struct DirectionalLightParam
 	{
+		// 位置
+		Vector3 position;
+
 		// 向き
 		Vector3 direction;
 
@@ -14,5 +17,52 @@ namespace Engine
 
 		// 色
 		Vector3 color;
+	};
+
+	/// @brief ポイントライトパラメータ
+	struct PointLightParam
+	{
+		// 位置
+		Vector3 position;
+
+		// 輝度
+		float intensity;
+
+		// 色
+		Vector3 color;
+
+		// ライトの届く距離
+		float radius;
+
+		// 減衰率
+		float decay;
+	};
+
+	/// @brief スポットライトパラメータ
+	struct SpotLightParam
+	{
+		// 位置
+		Vector3 position;
+
+		// 輝度
+		float intensity;
+
+		// 色
+		Vector3 color;
+
+		// 向き
+		Vector3 direction;
+
+		// ライトの届く距離
+		float distance;
+
+		// 減衰率
+		float decay;
+
+		// 光の当たる角度
+		float cosAngle;
+
+		// フォールオフ開始角度
+		float cosFalloffStart;
 	};
 }
