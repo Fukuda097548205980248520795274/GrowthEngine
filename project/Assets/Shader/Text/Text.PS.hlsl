e185bf7e3b637565d4caab0d52cf29366cb5fd56
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-03-20 14:35:49 +0900

    一応コミット

---
+++ project/Assets/Shader/Text/Text.PS.hlsl
@@ -0,0 +1,28 @@
+#include "../Sprite/Sprite.hlsli"
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+struct Material
+{
+    // 色
+    float4 color;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+
+Texture2D<float> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    
+    // テクスチャカラー
+    float textureColor = gTexture.Sample(gSampler, input.texcoord);
+    
+    output.color = gMaterial.color * textureColor;
+    
+    return output;
+}
