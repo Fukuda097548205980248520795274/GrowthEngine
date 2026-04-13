4353259e85980dbb8a20b18ee088d1de2d308465
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-04-13 13:39:14 +0900

    コミット

--- project/Game/Scene/GameScene/GameScene.cpp
+++ project/Game/Scene/GameScene/GameScene.cpp
@@ -11,9 +11,6 @@ void GameScene::Initialize()
 
 	// ライトを読み込む
 	engine_->LoadLight("DirectionalLight", Engine::Light::Type::Directional);
-
-	// パーティクル
-	engine_->LoadParticle3D("Particle_TEST", 1000, engine_->LoadModel("./Assets/Models/particlePlane", "particlePlane.obj"));
 }
 
 /// @brief 更新処理
@@ -27,7 +24,4 @@ void GameScene::Draw()
 {
 	// 地形を描画する
 	engine_->DrawRender3D("Walk");
-
-	// パーティクルを描画する
-	engine_->DrawParticle3D("Particle_TEST");
 }
