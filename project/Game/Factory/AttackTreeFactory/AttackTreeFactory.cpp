56979880e95156332f45fd9edb3ebb7e0f7e29c5
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-04-19 16:37:28 +0900

    一応コミット

---
+++ project/Game/Factory/AttackTreeFactory/AttackTreeFactory.cpp
@@ -0,0 +1,4 @@
+#include "AttackTreeFactory.h"
+#include "Entity/Character/Character.h"
+#include "Builder/BehaviorTreeBuilder/BehaviorTreeBuilder.h"
+
---
+++ project/Game/Factory/AttackTreeFactory/AttackTreeFactory.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Node/Node.h"
+
+class Character;
+
+class AttackTreeFactory
+{
+public:
+
+	
+};
+
