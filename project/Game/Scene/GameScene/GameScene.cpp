a31056da46a656f6c9fb4a326cf3c30b7d3a7ea1
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-05-26 13:31:34 +0900

    元に戻した。

--- project/Game/Scene/GameScene/GameScene.cpp
+++ project/Game/Scene/GameScene/GameScene.cpp
@@ -24,8 +24,6 @@ void GameScene::Initialize()
 {
 	engine_->LoadCamera3D("MainCamera");
 
-	engine_->LoadPostEffect("Smoothing", Engine::PostEffect::Type::Smoothing);
-
 	// 太陽光の生成と初期化
 	sunLight_ = std::make_unique<LightDirectional>("SunLight");
 
@@ -265,9 +263,6 @@ void GameScene::Draw()
 
 	// ポストエフェクトの描画処理
 	postEffectManager_->Draw(player_.get());
-
-
-	engine_->DrawPostEffect("Smoothing");
 
 }
 
